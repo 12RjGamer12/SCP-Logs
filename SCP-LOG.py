3a2c195c12eea61d1f94efd83deae71412e4992d
12RjGamer12 <102541014+12RjGamer12@users.noreply.github.com>
2022-03-28 12:10:17 -0400

    Add files via upload

---
+++ SCP-LOG.py
@@ -0,0 +1,41 @@
+
+
+
+password = input ("ENTER THE PASSWORD: ")
+
+if password == "APPLECAKE":
+    print("ACCESS GRANTED")
+
+
+else:
+    print("ACCESS DENIED")
+
+if password == "APPLECAKE":
+
+    key = input("PRESS ENTER TO CONENUE.")
+    print ("LOGS- WE ARE TRYING TO STUDY WETHER THE SCP FOUNDATION IS REAL OR NOT.")
+    
+    print("Would you like to see some evidence that the SCP Foundation is real? yes no. answer in yes or no, CASE SENSITIVE")
+
+    an = input("yes/no\n")
+
+if an == "yes":
+    print("We have found a fish corpse out in a field, it's organs and jaw where, gone. that could be evdince that SCP-3999 is real. would you like to close the program? yes/no\n")
+
+if  an == "no":
+    print("Would You like to know what SCP-31999 is?\n")
+ask = input("yes/no")
+if ask == "yes":
+    print("SCP-3199 is one of the most Dangerous SCP's on earth. SCP-3999's Name is: Humans, Refuted. that is a realy scary name. would you like to know what to do if you see an SCP-3999? ANSWER IN: yes or no, CASE SENSITIVE\n")
+else:
+    print("Would you like to close the program or contenue?")
+ask = input("yes = contenue, no = close the program")
+
+if ask == "yes":
+    print("If you see an SCP-3999 Do not run, it will easily out run you. Instead, Climb a tree, Make sure to keep eye contact, even though the may look increadible grose, it should not attac you, if tou hear screaming from inside a forest at night, do not panic. Leave the area imedentley. do not leave your house, do not go into the basement. contact the ofiritys.")
+else:
+    print ("OK, Would you like to close the porgram? yes/no case sensitive")
+    
+
+
+
