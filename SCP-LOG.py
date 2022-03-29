2803f1b3e67ad7e785fc72e5c317971075e18cdc
12RjGamer12 <102541014+12RjGamer12@users.noreply.github.com>
2022-03-29 17:50:29 -0400

    I just updated it

--- SCP-LOG.py
+++ SCP-LOG.py
@@ -17,24 +17,28 @@ if password == "APPLECAKE":
     
     print("Would you like to see some evidence that the SCP Foundation is real? yes no. answer in yes or no, CASE SENSITIVE")
 
-    an = input("yes/no\n")
+    yes = input("yes/no\n")
 
-if an == "yes":
+if yes == "yes":
     print("We have found a fish corpse out in a field, it's organs and jaw where, gone. that could be evdince that SCP-3999 is real. would you like to close the program? yes/no\n")
 
-if  an == "no":
+if  yes == "no":
     print("Would You like to know what SCP-31999 is?\n")
 ask = input("yes/no")
 if ask == "yes":
     print("SCP-3199 is one of the most Dangerous SCP's on earth. SCP-3999's Name is: Humans, Refuted. that is a realy scary name. would you like to know what to do if you see an SCP-3999? ANSWER IN: yes or no, CASE SENSITIVE\n")
 else:
     print("Would you like to close the program or contenue?")
-ask = input("yes = contenue, no = close the program")
+ask = input("yes = close program, no = contenue")
 
 if ask == "yes":
     print("If you see an SCP-3999 Do not run, it will easily out run you. Instead, Climb a tree, Make sure to keep eye contact, even though the may look increadible grose, it should not attac you, if tou hear screaming from inside a forest at night, do not panic. Leave the area imedentley. do not leave your house, do not go into the basement. contact the ofiritys.")
 else:
-    print ("OK, Would you like to close the porgram? yes/no case sensitive")
+    print ("OK, Would you like to close the porgram? yes/no case sensitive\n")
+    sh = input("yes/no\n")
+
+    if sh == "yes":
+           print("OK. Press Controll = C")
     
 
 
